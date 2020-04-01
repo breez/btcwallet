b8e9adbd60a4cab0236487d3c0e542f077e45b6a
Roei Erez <roeierez@gmail.com>
2020-04-01 16:15:20 +0300

    Merge branch 'lnd-merge-robust-birthday-search' into lnd-merge-0.9.2
    * lnd-merge-robust-birthday-search:
      wallet: keep searching for birthday in case header not found.

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -32,6 +32,7 @@ import (
 	"github.com/btcsuite/btcwallet/walletdb/migration"
 	"github.com/btcsuite/btcwallet/wtxmgr"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/lightninglabs/neutrino/headerfs"
 )
 
 const (
@@ -594,6 +595,10 @@ func locateBirthdayBlock(chainClient chainConn,
 		}
 		header, err := chainClient.GetBlockHeader(hash)
 		if err != nil {
+			if err == headerfs.ErrHashNotFound {
+				left = mid
+				continue
+			}
 			return nil, err
 		}
 
